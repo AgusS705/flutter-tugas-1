df25beb1648aa9d5e01687bc47811bf70c5f0393
Agus Sepriyandiansah <55836296+AgusS705@users.noreply.github.com>
2020-10-15 19:41:10 +0700

    Update about_page.dart

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:my_first_app/data.dart';
+import 'package:flutter/cupertino.dart'; 
+import 'package:flutter/material.dart'; //import library pihak ketiga atau package widgets
+import 'package:my_first_app/data.dart';  //import file data.dart
 
-Color kPrimaryColor = Color(0xffFFCC24);
-AssetImage kLogo = AssetImage('assets/images/logo.png');
-List<Person> listPerson = getPersonList();
+Color kPrimaryColor = Color(0xffFFCC24); //mendefinisakan kPrimaryColor
+AssetImage kLogo = AssetImage('assets/images/logo.png'); //mendefinisakan kLogo
+List<Person> listPerson = getPersonList(); //menambilkan getPersonList di file data.dart
 
-class AboutPage extends StatefulWidget {
+class AboutPage extends StatefulWidget { //membuat class aboutpage
   @override
   State<StatefulWidget> createState() => _AboutPageState();
 }
 
 class _AboutPageState extends State<AboutPage> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //widget utama
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
+      appBar: AppBar( //membuat appbar
+        backgroundColor: kPrimaryColor, //kPrimaryColor didapat oleh kPrimaryColor diatas
         elevation: 0,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
+        title: Column( //membuat title
+          crossAxisAlignment: CrossAxisAlignment.start, //menempatkan text di tengah layar
+          children: <Widget>[ //maksud dari children ialah didalamny terdapat lebih dari 1 widget
+            Text(  // membuat text
               "SDGs 7",
               style: TextStyle(
                 fontSize: 19,
@@ -29,8 +29,8 @@ class _AboutPageState extends State<AboutPage> {
                 color: Colors.black,
               ),
             ),
-            GestureDetector(
-                child: Text(
+            GestureDetector( //digunakan untuk mendeteksi gesture widget seperti ontap, doubletab dll.
+                child: Text( //maksud dari child ialah didalamny terdapat hanya 1 widget
               'Afforfable and Clean Energy',
               style: TextStyle(
                 fontSize: 15,
@@ -39,52 +39,52 @@ class _AboutPageState extends State<AboutPage> {
             ))
           ],
         ),
-        centerTitle: false,
-        actions: <Widget>[
-          Container(
-            margin: EdgeInsets.only(
+        centerTitle: false, //membuat centertitle tidak ditengah
+        actions: <Widget>[ //membuat action 
+          Container( //membuat wadah kLogo
+            margin: EdgeInsets.only( //Membuat margin dimana semua offset bernilai
               right: 16,
               top: 8,
             ),
             width: 50,
-            child: CircleAvatar(
-              backgroundImage: kLogo,
+            child: CircleAvatar( //membuat avatar berbentuk lingkaran
+              backgroundImage: kLogo, //kLogo didapat oleh kLogo diatas
             ),
           ),
         ],
       ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
+      body: Stack( //membuat body dan stack untuk mengatur tatak letak secara custom
+        children: <Widget>[ //maksud dari children ialah didalamny terdapat lebih dari 1 widget
+          Positioned( //mengontrol posisi widget
               top: 0,
-              child: Container(
+              child: Container( //membuat wadah
                 width: MediaQuery.of(context).size.width,
                 height: 210,
-                color: kPrimaryColor,
+                color: kPrimaryColor, //kPrimaryColor didapat oleh kPrimaryColor diatas
                 padding: EdgeInsets.only(top: 20, right: 23, left: 23),
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
+                child: Column( //membuat colom
+                  children: <Widget>[ //maksud dari children ialah didalamny terdapat lebih dari 1 widget
+                    Row( //untuk menempatkan widget- widget lainnya secara horizontal
+                      mainAxisAlignment: MainAxisAlignment.start, //membuat colom nempel di pinggir
+                      children: <Widget>[ //maksud dari children ialah didalamny terdapat lebih dari 1 widget
+                        Container( //membuat wadah logo
                           margin: EdgeInsets.only(right: 16),
-                          child: Image(
+                          child: Image( //maksud dari child ialah didalamny terdapat hanya 1 widget img
                             image: AssetImage('assets/images/logo.png'),
                             width: 71,
                           ),
                         ),
-                        Container(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
+                        Container( //membuat wadah text
+                          child: Column( //maksud dari child ialah didalamny terdapat hanya 1 widget colom
+                            mainAxisAlignment: MainAxisAlignment.center, //membuat posisi text center
+                            crossAxisAlignment: CrossAxisAlignment.start, ////membuat colom nempel di pinggir
+                            children: <Widget>[ //maksud dari children ialah didalamny terdapat lebih dari 1 widget
+                              Text( //membbuat text
                                 'SDGs 7',
                                 style: TextStyle(
                                     fontSize: 23, fontWeight: FontWeight.bold),
                               ),
-                              Text(
+                              Text( //membbuat text
                                 'Afforfable and Clean Energy',
                                 style: TextStyle(
                                     fontSize: 18,
